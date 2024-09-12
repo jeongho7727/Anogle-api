9f4d4f4089513eb10f7ea60130987085e6fa89a2
jeongho7727 <jeangho293@gmail.com>
2024-09-12 20:23:20 +0900

    feat: auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,12 +4,14 @@ import type { DddContext } from '../libs/ddd';
 import { UserRepository } from '../services/users/infrastructure/repository';
 import { FilteredUserSpec } from '../services/users/domain/specs';
 import { docs } from '../config';
+import { badRequest, unauthorized } from '@hapi/boom';
 
 export const authMiddleware = async (ctx: Context, next: Next) => {
   const accessToken = ctx.request.headers['authorization'] || '';
 
   if (!accessToken) {
     // TODO: 에러 인증 오류
+    throw unauthorized('There is no access token.');
   }
 
   const decoded = verify(accessToken, docs.jwtSecret) as { userId: number };
@@ -17,9 +19,12 @@ export const authMiddleware = async (ctx: Context, next: Next) => {
   const { context } = ctx.state as { context: DddContext };
   const userRepository = context.get(UserRepository);
 
-  try {
-    const user = await userRepository.findSatisfying(new FilteredUserSpec({ id: decoded.userId }));
+  const user = await userRepository.findSatisfying(new FilteredUserSpec({ id: decoded.userId }));
 
-    await next();
-  } catch (err) {}
+  if (!user) {
+    throw badRequest('No user.', { message: 'No user' });
+  }
+
+  ctx.state.userId = decoded.userId;
+  await next();
 };
